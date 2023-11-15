199ee0bc4776a6d63dd9e797a9aac117862c89ff
Tate Capra <tatecapra@outlook.com>
2023-11-15 15:00:00 -0500

    Working on SQL Tables and Writing pseudocode

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+// Button Submit for page login
+// Create Msg if Login information is incorrect to resubmit 
+
+
+//Load Main Page 
---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// Main Page Connected to DB
+
+// Pull in CLickUp API
+// Display Data 
+
+
+//Filter Functionality when creating the pull request? or pull all data
+    //then a filter was is displayed based on a boolean value?
