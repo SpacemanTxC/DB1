f1c074cf17aba04fbeb06509a9b1da26955d5f28
Tate Capra <tatecapra@outlook.com>
2023-11-18 14:49:46 -0500

    Adding notes and html seciton for the main page after loading

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,10 @@
 // Button Submit for page login
+    // Click Fuction
 // Create Msg if Login information is incorrect to resubmit 
+
+// Number of tries before logout? 
 
 
 //Load Main Page 
+// Found under main html
+// React will change each sections.
